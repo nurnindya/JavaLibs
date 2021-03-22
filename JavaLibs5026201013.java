f41e4b22244b0e0b74e8d44048921f62831bf114
runaydnin <81071026+runaydnin@users.noreply.github.com>
2021-03-22 21:14:33 +0700

    Add files via upload

---
+++ JavaLibs5026201013.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class JavaLibs5026201013 {
+
+public static void main(String[] args) {
+String question1 = JOptionPane.showInputDialog("What's your full name?");
+String question2 = JOptionPane.showInputDialog("What's your nick name?");
+String question3String = JOptionPane.showInputDialog("How old are you?");
+int question3 = Integer.parseInt(question3String);
+String question4String = JOptionPane.showInputDialog("How tall are you?");
+int question4 = Integer.parseInt(question4String);
+String question5 = JOptionPane.showInputDialog("What's your favorite food?");
+String question6 = JOptionPane.showInputDialog("What's your favorite drink?");
+String question7 = JOptionPane.showInputDialog("What's your hobby?");
+String question8String = JOptionPane.showInputDialog("How many money in your pocket now?");
+double question8 = Double.parseDouble(question8String);
+String question9int = JOptionPane.showInputDialog("5 + 3 =");
+String question10double = JOptionPane.showInputDialog("0.7 - 0.5 = ");
+
+    }
+}
